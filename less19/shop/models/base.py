c04f3ad5265ee52d1e3eb782d6b15a25b72364cb
Kirill <makybex11@gmail.com>
2024-10-07 19:09:32 +0300

    less 25

--- less19/shop/models/base.py
+++ less19/shop/models/base.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
 # Create an engine this will create a new sqlite database in current workingdir
-engin = create_engine("postgresql://kirill:kirill@127.0.0:5432/shop")
-engine = create_engine("sqlite:///shop.sqlite", echo=True)
+engine = create_engine("postgresql://postgres:102030@127.0.0.1:5432/postgres")
+# engine = create_engine("sqlite:///shop.sqlite", echo=True)
 
 # Define a base class using declarative_base
 Base = declarative_base()
